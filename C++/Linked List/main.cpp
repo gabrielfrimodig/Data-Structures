fb1d6bfe393064eab7af87d9ce69b8c292c935f4
gabrielfrimodig <gw@pop-os.localdomain>
2021-12-29 13:34:46 +0100

    Size of Linked List

--- C++/Linked List/main.cpp
+++ C++/Linked List/main.cpp
@@ -7,12 +7,24 @@ public:
     int data;
 };
 
+int sizeLinkedList(Node *n)
+{
+    int counter = 0;
+
+    while (n != nullptr)
+    {
+        counter++;
+        n = n->next;
+    }
+
+    return counter;
+}
+
 int main()
 {
     Node *head = new Node();
-
-    head->next = nullptr;
-    head->data = 10;
+    head->next = sec;
+    head->data = 5;
 
     return 0;
 }
